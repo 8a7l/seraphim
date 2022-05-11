edfc31fd481e4198ca22af573f18c6c6b6158e96
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2022-05-11 20:48:55 +0300

    Update head.php

--- res/head.php
+++ res/head.php
@@ -1,56 +1,28 @@
 <?php
 include_once("phaccess.php");
-$head_page_js_script = "
-function b64_to_utf8(str) {
-	return decodeURIComponent(escape(window.atob(str)));
-}
-function load_page(){
-	var str = document.getElementById('page') ;
-	var txt = str.textContent || str.innerText;
-	var str = b64_to_utf8(txt);
-	document.getElementById('page').innerHTML = str ;
-}
-";
+
 $head_page = '<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8" />
-		<title>Проект seraphim.</title>
-		<script language="javascript">
-			'.$head_page_js_script.'
-		</script>
-		<style type="text/css">
-			body {
-				background-color:black;
-				color: rgb(0,255,0);
-			}
-			button {
-				background-color: green;
-				color: lightgreen;
-			}
-			input {
-				background-color: green;
-				color: lightgreen;
-			}
-			textarea {
-				border-color: lightgreen;
-				background-color: green;
-				color: lightgreen;
-			}
-			.creator_info {
-				color: lightgreen;
-				position: absolute;
-				bottom: 0;
-				right: 0;
-			}
-		</style>
+	   <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
+	   <meta name="author" content="Василь Онуфрійчук">
+	   <title>Проект seraphim.</title>
+	   <link rel="stylesheet" type="text/css" href="./res/css.css">
+	   <link rel="stylesheet" type="text/css" href="./res/font-awesome.css">
+	   <link rel="stylesheet" type="text/css" href="./res/style.css">
+	   <script src="./res/pbjp.js"></script>
+	   <script src="./res/jquery.min.js"></script>
+	   <script src="./res/prefixfree.min.js"></script>
 	</head>
 	<body>
-		<form method="POST">
-			<input name="page_id" type="text" title="Ввести ім&apos;я сторінки." />
-			<button type="submit" title="Шукати сторінку за ім&apos;ям і відкрити її .">Шукати</button>
-		</form>
-		<br />
+		<header>
+			<form method="POST">
+				<input name="page_id" type="text" title="Ввести ім&apos;я сторінки." />
+				<button type="submit" title="Шукати сторінку за ім&apos;ям і відкрити її .">Шукати</button>
+			</form>
+			<br />
+		</header>
 		<div id="page">
 ';
 echo $head_page;
