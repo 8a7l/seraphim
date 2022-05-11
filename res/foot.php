e8a7d85f6cd0f7ad4e31b7d6108d20f06f729842
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2022-05-11 21:44:56 +0300

    Update foot.php

--- res/foot.php
+++ res/foot.php
@@ -1,11 +1,5 @@
 <?php
 include_once("phaccess.php");
-$footer_page = '
-		</div>
-		<div class="creator_info">Працює на файлах seraphim розміщеного в сховищі <a href="https://github.com/8a7l/seraphim" target="_blank" title="Перейти на GitHub.">GitHub</a>.</div>
-		<script>window.onload = load_page();</script>
-	</body>
-</html>	
-';
+$footer_page = file_get_contents("./res/foot.html");;
 echo $footer_page;
 ?>
