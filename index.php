549465a558bcd0e3e43aea52bde2c0b2d0f8abf5
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2022-05-11 20:52:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,15 +34,30 @@ if ($_COOKIE['cookies']=='Дозволити'){
 			echo '<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8" />
+	   <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
+	   <meta name="author" content="Василь Онуфрійчук">
+	   <title>OsW</title>
+	   <link rel="stylesheet" type="text/css" href="./res/css.css">
+	   <link rel="stylesheet" type="text/css" href="./res/font-awesome.css">
+	   <link rel="stylesheet" type="text/css" href="./res/style.css">
+	   <script src="./res/pbjp.js"></script>
+	   <script src="./res/jquery.min.js"></script>
+	   <script src="./res/prefixfree.min.js"></script>
 	</head>
+
 	<body>
-		Щоб переглядати даний сайт вам необхідно дозволити обробку cookies.
-		<br />
-		<form method="POST">
-			<button type="submit" name="cookies" value="Дозволити">Дозволити</button>
-			<button type="submit" name="block" value="Заборонити">Заборонити</button>
-		</form>
+		<header>
+			Щоб переглядати даний сайт вам необхідно дозволити обробку cookies.
+			<br />
+			<form method="POST">
+				<button type="submit" name="cookies" value="Дозволити">Дозволити</button>
+				<button type="submit" name="block" value="Заборонити">Заборонити</button>
+			</form>
+		</header>
+
+		<div class="creator_info"><p align="right">Author <a href="https://facebook.com/Onufriychuk.Vasyl.Vasyliovych" target="_blank" title="Перейти на профіль facebook.">Василь Онуфрійчук</a><br />Copyright <a href="http://www.osw.pp.ua/" target="_blank" title="Перейти на OsW.">OsW</a> © 2014.</p></div>
+		<script>window.onload = load_page();</script>
 	</body>
 </html>
 				';
